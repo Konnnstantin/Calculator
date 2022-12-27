e11c09b94b63b6d439e44afb72f3406f81f9544b
Konstantin <kurakin-1993@mail.ru>
2022-12-27 22:39:03 +0400

    Add project files.

---
+++ Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace Calculator.Tests
+{
+    public class CalculatorTest
+    {
+        Calculator calculator = new Calculator();
+
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Assert.That(calculator.Additional(20, 10) == 30);
+        }
+
+        [Test]
+        public void SubstractionMustReturnCorrectValue()
+        {
+            Assert.True(calculator.Subtraction(20, 10) == 10);
+
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectionValue()
+        {
+            Assert.True(calculator.Miltiplication(20, 10) == 200);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectionValue()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(20, 0));
+            Assert.True(calculator.Division(20, 10) == 2);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
